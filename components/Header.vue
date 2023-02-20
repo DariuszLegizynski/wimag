54d6218e82059602779e9c097edd1eca5e381167
Dariusz Legizynski <dariuszlegizynski@lemon42.com>
2023-02-20 15:16:56 +0100

    add scs style files

--- components/Header.vue
+++ components/Header.vue
@@ -31,5 +31,80 @@
 </template>
 
 <style lang="scss" scoped>
+@import '@/assets/scss/_general.scss';
 
+.header {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+
+  position: fixed;
+  top: 0;
+  padding-right: 1rem;
+
+  width: 100vw;
+  height: auto;
+
+  z-index: 10;
+
+  background-color: transparent;
+  -webkit-backdrop-filter: blur(3px);
+  backdrop-filter: blur(3px);
+
+  &__active {
+    -webkit-backdrop-filter: none;
+    backdrop-filter: none;
+    background-color: transparent !important;
+    box-shadow: none !important;
+  }
+
+  &__scroll {
+    background-color: $primary-opacity;
+    box-shadow: 0 4px 8px rgba(0, 0, 0, 0.2);
+  }
+  .logo {
+    & > img {
+      width: 3rem;
+      height: auto;
+      background-color: $color-primary;
+      padding: 0.4rem;
+    }
+  }
+  nav {
+    display: none;
+    visibility: hidden;
+  }
+}
+
+@media only screen and (min-width: 640px) {
+  .header {
+    nav {
+      display: flex;
+      visibility: visible;
+      justify-content: space-around;
+      align-items: center;
+      width: 57%;
+
+      .btn {
+        margin: 0;
+        padding: 0 0.4rem;
+        width: auto;
+        font-size: $font-size-18;
+        line-height: $line-height-18;
+      }
+    }
+  }
+}
+@media only screen and (min-width: 1024px) {
+  .header {
+    nav {
+      width: 80%;
+
+      .btn {
+        font-size: $font-size-24;
+        line-height: $line-height-30;
+      }
+    }
+  }
+}
 </style>
